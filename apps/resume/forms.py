d2c6bdb50e720a88d5009e116dc74f25e88e21b8
Daniel <lucha0075.db@gmail.com>
2019-03-08 22:27:27 +0000

    add new for class for Resume Model

--- apps/resume/forms.py
+++ apps/resume/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 
-from models import ResumeItem
-
+from models import ResumeItem, Resume
 
 class ResumeItemForm(forms.ModelForm):
     """
     A form for creating and editing resume items. Note that 'user' is not
     included: it is always set to the requesting user.
     """
+
     class Meta:
         model = ResumeItem
         fields = ['title', 'company', 'start_date', 'end_date', 'description']
+
+class ResumeForm(forms.ModelForm):
+    """
+    A form for creating and editing a resume. Note that 'user' is not
+    included: it is always set to the requesting user.
+    """
+    class Meta:
+        model = Resume
+        fields = ['title']
