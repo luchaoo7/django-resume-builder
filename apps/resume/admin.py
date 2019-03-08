71ad3f978c8098ef96fce64b8172ea2404d6b207
Daniel <lucha0075.db@gmail.com>
2019-03-08 22:17:21 +0000

    registered Resume model to addmin

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -5,5 +5,12 @@ from . import models
 
 @admin.register(models.ResumeItem)
 class ResumeItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'company', 'start_date')
+    list_display = ('user', 'title', 'company', 'start_date', 'resume')
     ordering = ('user', '-start_date')
+
+@admin.register(models.Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+
+    class Meta:
+        model = models.Resume
